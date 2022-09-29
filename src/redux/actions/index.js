9c4439cc624b081a7e1fc470f6403eb54de13f33
wendel-D <wendeldaniel263@gmail.com>
2022-09-29 16:13:05 -0300

    redux parcialmente configurado

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,13 @@
 // Coloque aqui suas actions
+export const ADD_USER = 'ADD_USER';
+
+export const userInfo = () => ({
+    type: ADD_USER,
+});
+
+export const ADD_WALLET = 'ADD_WALLET';
+
+export const walletInfo = () => ({
+    type: ADD_WALLET,
+});
+
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,8 @@
-// configure aqui sua store
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension/lib/types/logOnly';
+import rootReducer from './reducers'
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
