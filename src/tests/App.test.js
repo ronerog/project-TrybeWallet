bc5cf021061e75ab1e1b8864310e4e1abe43cbbf
wendel-D <wendeldaniel263@gmail.com>
2022-10-05 14:45:15 -0300

    correção de lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,20 +5,20 @@ import { renderWithRouterAndRedux } from './helpers/renderWith';
 import App from '../App';
 
 describe('Test Coverage', () => {
-    it('Render check', () => {
-      const { history } = renderWithRouterAndRedux(<App />);
-      const emailCorrect = 'test@test.com';
-      const passwordCorrect = '123456';
-      const buttonEntrar = screen.getByRole('button', { name: /entrar/i });
-      const inputPassword = screen.getByTestId('password-input');
-      const inputEmail = screen.getByTestId('email-input');
-      expect(buttonEntrar).toBeInTheDocument();
-      expect(inputEmail).toBeInTheDocument();
-      expect(inputPassword).toBeInTheDocument();
-      userEvent.type(inputEmail, emailCorrect);
-      userEvent.type(inputPassword, passwordCorrect);
-      userEvent.click(buttonEntrar);
-      const { location: { pathname } } = history;
-      expect(pathname).toBe('/carteira');
-    });
+  it('Render check', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const emailCorrect = 'test@test.com';
+    const passwordCorrect = '123456';
+    const buttonEntrar = screen.getByRole('button', { name: /entrar/i });
+    const inputPassword = screen.getByTestId('password-input');
+    const inputEmail = screen.getByTestId('email-input');
+    expect(buttonEntrar).toBeInTheDocument();
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputPassword).toBeInTheDocument();
+    userEvent.type(inputEmail, emailCorrect);
+    userEvent.type(inputPassword, passwordCorrect);
+    userEvent.click(buttonEntrar);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/carteira');
   });
+});
